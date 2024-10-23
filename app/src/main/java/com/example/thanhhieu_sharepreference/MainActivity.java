ad387380a29386f0463e5a11877eece9355606ad
Thanh Hieu <thanhhieu8304@gmail.com>
2024-10-23 09:34:45 +0700

    Update function: Get files dataRating from ShareReference

--- app/src/main/java/com/example/thanhhieu_sharepreference/MainActivity.java
+++ app/src/main/java/com/example/thanhhieu_sharepreference/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.thanhhieu_sharepreference;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -36,31 +37,27 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         initComponents();
-        SharedPreferences sharedPref = this.getSharedPreferences(
+        SharedPreferences sharedPref = getSharedPreferences(
                 "dataRating", Context.MODE_PRIVATE);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
                 if (checkBox.isChecked()){
-                    SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString("name", String.valueOf(name));
-                    editor.putString("classText", String.valueOf(classText));
+                    Log.e("CHECKBOX CHECKED!", "HIHI");
+                    editor.putString("name", name.getText().toString());
+                    editor.putString("classText",  classText.getText().toString());
                     editor.putBoolean("checkBox", checkBox.isChecked());
-                    editor.putInt("ratingBar", ratingBar.getNumStars());
-                    editor.apply();
-                    editor.commit();
+                    editor.putFloat("ratingBar", ratingBar.getRating());
                 }
                 else {
-                    SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
+                    Log.e("UNCHECKED!", "Heee00Hee");
                     editor.remove("name");
                     editor.remove("classText");
                     editor.remove("checkBox");
                     editor.remove("ratingBar");
-                    editor.apply();
-                    editor.commit();
                 }
+                editor.apply();
                 loadSavedData(sharedPref);
             }
         });
@@ -70,12 +67,12 @@ public class MainActivity extends AppCompatActivity {
         String savedName = sharedPref.getString("name", "");
         String savedClassText = sharedPref.getString("classText", "");
         boolean savedCheckBox = sharedPref.getBoolean("checkBox", false);
-        int savedRating = sharedPref.getInt("ratingBar", 0);
-
+        float savedRating = sharedPref.getFloat("ratingBar", 0.0f);
+        Log.e("Saved Name", savedName);
         name.setText(savedName);
         classText.setText(savedClassText);
         checkBox.setChecked(savedCheckBox);
-        ratingBar.setNumStars(savedRating);
+        ratingBar.setRating(savedRating);
     }
 
     private void initComponents() {
@@ -83,6 +80,6 @@ public class MainActivity extends AppCompatActivity {
         classText = findViewById(R.id.editTextPassword);
         checkBox = findViewById(R.id.checkBox);
         ratingBar = findViewById(R.id.ratingBar);
-        saveButton = findViewById(R.id.button2);
+        saveButton = findViewById(R.id.saveButton);
     }
 }
